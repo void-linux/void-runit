387f205f49b115694727e60290da9674d15efb56
Juan RP <xtraeme@gmail.com>
2014-08-08 01:54:56 +0200

    core-services/00-pseudofs.sh: do not mount cgroupfs on virtualization.

--- core-services/00-pseudofs.sh
+++ core-services/00-pseudofs.sh
@@ -8,5 +8,8 @@ mountpoint -q /dev || mount -t devtmpfs dev /dev -o mode=0755,nosuid
 mkdir -p -m0755 /run/runit /run/lvm /run/user /dev/pts /dev/shm
 mountpoint -q /dev/pts || mount -n -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
 mountpoint -q /dev/shm || mount -n -t tmpfs shm /dev/shm -o mode=1777,nosuid,nodev
-mountpoint -q /sys/fs/cgroup || mount -t tmpfs cgroup /sys/fs/cgroup -o mode=0755
-awk '$4==1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || mount -t cgroup -o " $1 ",x-mount.mkdir cgroup /sys/fs/cgroup/" $1) }' /proc/cgroups
+
+if [ -z "$VIRTUALIZATION" ]; then
+    mountpoint -q /sys/fs/cgroup || mount -t tmpfs cgroup /sys/fs/cgroup -o mode=0755
+    awk '$4==1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || mount -t cgroup -o " $1 ",x-mount.mkdir cgroup /sys/fs/cgroup/" $1) }' /proc/cgroups
+fi
