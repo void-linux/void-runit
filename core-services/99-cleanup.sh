9ad55852748d7dd2f10fc14edeb18e23d0ddd277
Juan RP <xtraeme@gmail.com>
2014-08-28 12:39:16 +0200

    core-services/99-cleanup.sh: fixed permissions for {b,u}tmp.

--- core-services/99-cleanup.sh
+++ core-services/99-cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-install -m0644 -o root -g utmp /dev/null /run/utmp
+install -m0664 -o root -g utmp /dev/null /run/utmp
 if [ ! -e /var/log/wtmp ]; then
 	install -m0664 -o root -g utmp /dev/null /var/log/wtmp
 fi
 if [ ! -e /var/log/btmp ]; then
-	install -m0664 -o root -g utmp /dev/null /var/log/btmp
+	install -m0600 -o root -g utmp /dev/null /var/log/btmp
 fi
 install -dm1777 /tmp/.X11-unix /tmp/.ICE-unix
 rm -f /etc/nologin /forcefsck /forcequotacheck /fastboot
