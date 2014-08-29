38934cf5018334d73bcea194845e3ae3fcc94bda
Juan RP <xtraeme@gmail.com>
2014-08-29 19:46:03 +0200

    core-services/05-misc.sh: initialize random seed earlier.

--- core-services/05-misc.sh
+++ core-services/05-misc.sh
@@ -1,5 +1,9 @@
 # vim: set ts=4 sw=4 et:
 
+msg "Initializing random seed...\n"
+cp /var/lib/random-seed /dev/urandom >/dev/null 2>&1 || true
+( umask 077; dd if=/dev/urandom of=/var/lib/random-seed count=1 bs=512 >/dev/null 2>&1 )
+
 msg "Setting up loopback interface...\n"
 ip link set up dev lo
 
@@ -15,7 +19,3 @@ if [ -n "$TIMEZONE" ]; then
     msg "Setting up timezone to '${TIMEZONE}'...\n"
     ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 fi
-
-msg "Initializing random seed...\n"
-cp /var/lib/random-seed /dev/urandom >/dev/null 2>&1 || true
-( umask 077; dd if=/dev/urandom of=/var/lib/random-seed count=1 bs=512 >/dev/null 2>&1 )
