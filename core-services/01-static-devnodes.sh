2ba8f93b1c87f179a362537a1da39ba2b2140d79
Juan RP <xtraeme@voidlinux.eu>
2015-04-24 08:23:07 +0200

    core-services: load kernel modules that aren't autoloaded by eudev3 anymore.
    We use the output of `kmod static-nodes` to get a list of modules that
    should be loaded for the running kernel, just like systemd does via tmpfiles.d(5).

---
+++ core-services/01-static-devnodes.sh
@@ -0,0 +1,6 @@
+# Some kernel modules must be loaded before starting udev(7).
+# Load them by looking at the output of `kmod static-nodes`.
+
+for f in $(kmod static-nodes 2>/dev/null|awk '/Module/ {print $2}'); do
+	modprobe -q $f 2>/dev/null
+done
