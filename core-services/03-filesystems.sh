ab7f9c1f98d63c9e39008a8a2ab138093c82ce12
Dominik Honnef <dominik@honnef.co>
2016-05-25 20:38:48 +0200

    Import and mount ZFS at boot time (#46)

--- core-services/03-filesystems.sh
+++ core-services/03-filesystems.sh
@@ -30,6 +30,22 @@ if [ -e /etc/crypttab ]; then
     fi
 fi
 
+if [ -e /etc/zfs/zpool.cache -a -x /usr/bin/zfs ]; then
+    msg "Activating ZFS devices..."
+    zpool import -c /etc/zfs/zpool.cache -N -a
+
+    msg "Mounting ZFS file systems..."
+    zfs mount -a
+
+    msg "Sharing ZFS file systems..."
+    zfs share -a
+
+    # NOTE(dh): ZFS has ZVOLs, block devices on top of storage pools.
+    # In theory, it would be possible to use these as devices in
+    # dmraid, btrfs, LVM and so on. In practice it's unlikely that
+    # anybody is doing that, so we aren't supporting it for now.
+fi
+
 [ -f /fastboot ] && FASTBOOT=1
 [ -f /forcefsck ] && FORCEFSCK="-f"
 for arg in $(cat /proc/cmdline); do
