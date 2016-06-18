5a920a4d15bbd6cae640205e545a4f4b0aa81ab7
Duncaen <mail@duncano.de>
2016-06-18 04:01:34 +0200

    00-pseudofs.sh: create /run/lock and /run/log

--- core-services/00-pseudofs.sh
+++ core-services/00-pseudofs.sh
@@ -5,7 +5,7 @@ mountpoint -q /proc || mount -o nosuid,noexec,nodev -t proc proc /proc
 mountpoint -q /sys || mount -o nosuid,noexec,nodev -t sysfs sys /sys
 mountpoint -q /run || mount -o mode=0755,nosuid,nodev -t tmpfs run /run
 mountpoint -q /dev || mount -o mode=0755,nosuid -t devtmpfs dev /dev
-mkdir -p -m0755 /run/runit /run/lvm /run/user /dev/pts /dev/shm
+mkdir -p -m0755 /run/runit /run/lvm /run/user /run/lock /run/log /dev/pts /dev/shm
 mountpoint -q /dev/pts || mount -o mode=0620,gid=5,nosuid,noexec -n -t devpts devpts /dev/pts
 mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/shm
 
