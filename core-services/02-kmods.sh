516e23ca257fb28e745e409cb73ffed30c61e6f9
Jan Tatje <jan@jnt.io>
2016-09-02 02:21:21 +0200

    Do not try to load modules if kernel doesn't support modules.

--- core-services/02-kmods.sh
+++ core-services/02-kmods.sh
@@ -1,6 +1,8 @@
 # vim: set ts=4 sw=4 et:
 
 [ -n "$VIRTUALIZATION" ] && return 0
+# Do not try to load modules if kernel does not support them.
+[ ! -e /proc/modules ] && return 0
 
 msg "Loading kernel modules..."
 modules-load -v | tr '\n' ' ' | sed 's:insmod [^ ]*/::g; s:\.ko\(\.gz\)\? ::g'
