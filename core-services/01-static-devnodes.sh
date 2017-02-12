af8113785f717f7f6ecb57b18904b98845aa0a21
Daniel Néri <dne+github@mayonnaise.net>
2017-02-12 01:21:06 +0100

    core-services/01-static-devnodes.sh: Honour modprobe blacklists

--- core-services/01-static-devnodes.sh
+++ core-services/01-static-devnodes.sh
@@ -2,5 +2,5 @@
 # Load them by looking at the output of `kmod static-nodes`.
 
 for f in $(kmod static-nodes 2>/dev/null|awk '/Module/ {print $2}'); do
-	modprobe -q $f 2>/dev/null
+	modprobe -bq $f 2>/dev/null
 done
