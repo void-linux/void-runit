fae8defa213dbeed39379b92144ae5a1e302e089
Olivier Mauras <olivier@mauras.ch>
2017-03-14 16:54:23 +0100

    core-services/00-pseudofs.sh: Add securityfs mountpoint - Needed for LSM support

--- core-services/00-pseudofs.sh
+++ core-services/00-pseudofs.sh
@@ -8,6 +8,7 @@ mountpoint -q /dev || mount -o mode=0755,nosuid -t devtmpfs dev /dev
 mkdir -p -m0755 /run/runit /run/lvm /run/user /run/lock /run/log /dev/pts /dev/shm
 mountpoint -q /dev/pts || mount -o mode=0620,gid=5,nosuid,noexec -n -t devpts devpts /dev/pts
 mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/shm
+mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
 
 if [ -z "$VIRTUALIZATION" ]; then
     mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
