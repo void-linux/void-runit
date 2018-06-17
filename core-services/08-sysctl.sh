48a8852708ea3efada94a9165b4f883034d99c07
Nicolas Porcel <nicolasporcel06@gmail.com>
2018-06-17 03:47:53 +0200

    core-services/sysctl: allow system file masking
    This aligns the behavior of this script with `sysctl --system`. If a
    file is present in 2 directory, only the first one will be read.
    
    For instance, this allows the user to overwrite
    /usr/lib/sysctl.d/void.conf by creating the file /etc/sysctl.d/void.conf.
    In that case, only the later will be read.

--- core-services/08-sysctl.sh
+++ core-services/08-sysctl.sh
@@ -2,15 +2,19 @@
 
 if [ -x /sbin/sysctl -o -x /bin/sysctl ]; then
     msg "Loading sysctl(8) settings..."
+    mkdir /run/sysctl
     for i in /run/sysctl.d/*.conf \
         /etc/sysctl.d/*.conf \
         /usr/local/lib/sysctl.d/*.conf \
         /usr/lib/sysctl.d/*.conf \
         /etc/sysctl.conf; do
 
-        if [ -e "$i" ]; then
+        fname="/run/sysctl/${i##*/}"
+        if [ -e "$i" ] && [ ! -e "$fname" -o "$i" = "/etc/sysctl.conf" ]; then
             printf '* Applying %s ...\n' "$i"
             sysctl -p "$i"
+            touch "$fname"
         fi
     done
+    rm -rf /run/sysctl
 fi
