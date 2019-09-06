047744cc50cdc2c84cc15374fa87a194ef63d384
Leah Neukirchen <leah@vuxu.org>
2019-09-06 11:19:26 +0200

    halt: style

--- halt.c
+++ halt.c
@@ -38,13 +38,13 @@ void write_wtmp(int boot) {
 
   utmp.ut_type = boot ? BOOT_TIME : RUN_LVL;
 
-  strncpy(utmp.ut_name, boot ? "reboot" : "shutdown", sizeof(utmp.ut_name));
-  strncpy(utmp.ut_id , "~~", sizeof(utmp.ut_id));
-  strncpy(utmp.ut_line, boot ? "~" : "~~", sizeof(utmp.ut_line));
+  strncpy(utmp.ut_name, boot ? "reboot" : "shutdown", sizeof utmp.ut_name);
+  strncpy(utmp.ut_id , "~~", sizeof utmp.ut_id);
+  strncpy(utmp.ut_line, boot ? "~" : "~~", sizeof utmp.ut_line);
   if (uname(&uname_buf) == 0)
-    strncpy(utmp.ut_host, uname_buf.release, sizeof(utmp.ut_host));
+    strncpy(utmp.ut_host, uname_buf.release, sizeof utmp.ut_host);
 
-  write(fd, (char *)&utmp, sizeof(utmp));
+  write(fd, (char *)&utmp, sizeof utmp);
   close(fd);
 
   if (boot) {
