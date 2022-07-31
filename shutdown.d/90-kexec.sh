79675bcb1a3c926f30415f8fa30c8bee8c9360b9
Philipp David <pd@3b.pm>
2022-07-31 16:27:01 +0200

    shutdown.d/90-kexec.sh: only try to run kexec on reboot

--- shutdown.d/90-kexec.sh
+++ shutdown.d/90-kexec.sh
@@ -1,5 +1,5 @@
 if [ -z "$VIRTUALIZATION" ]; then
-    if [ -e /run/runit/reboot ] && command -v kexec >/dev/null; then
+    if [ -x /run/runit/reboot ] && command -v kexec >/dev/null; then
         msg "Triggering kexec..."
         kexec -e 2>/dev/null
         # not reached when kexec was successful.
