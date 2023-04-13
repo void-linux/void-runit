1d453575469f396e1ca8dd5cc645e1af4e18dc8e
classabbyamp <void@placeviolette.net>
2023-04-13 00:00:07 -0400

    core-services/01-static-devnodes: simplify parsing
    fixes #82

--- core-services/01-static-devnodes.sh
+++ core-services/01-static-devnodes.sh
@@ -1,6 +1,6 @@
 # Some kernel modules must be loaded before starting udev(7).
 # Load them by looking at the output of `kmod static-nodes`.
 
-for f in $(kmod static-nodes 2>/dev/null|awk '/Module/ {print $2}'); do
+for f in $(kmod static-nodes -f devname 2>/dev/null|cut -d' ' -f1); do
 	modprobe -bq $f 2>/dev/null
 done
