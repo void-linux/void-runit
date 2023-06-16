329b891426efc093ad8c65e96364427c2fb25b54
Leah Neukirchen <leah@vuxu.org>
2023-06-16 13:58:06 +0200

    core-services/99-cleanup.sh: also create lastlog

--- core-services/99-cleanup.sh
+++ core-services/99-cleanup.sh
@@ -6,5 +6,8 @@ fi
 if [ ! -e /var/log/btmp ]; then
 	install -m0600 -o root -g utmp /dev/null /var/log/btmp
 fi
+if [ ! -e /var/log/lastlog ]; then
+	install -m0600 -o root -g utmp /dev/null /var/log/lastlog
+fi
 install -dm1777 /tmp/.X11-unix /tmp/.ICE-unix
 rm -f /etc/nologin /forcefsck /forcequotacheck /fastboot
