066e9077c1865f4273c6c706b7e509e72742ce80
Leah Neukirchen <leah@vuxu.org>
2025-03-09 17:05:45 +0100

    10-runit-control: make a hardlink to the currently booted init
    This ensures the link count of the pid 1 executable remains positive
    when runit is updated.  If the link count is zero, the file system
    cannot be remounted read-only during shutdown, as the disk space needs
    to be reclaimed.

--- core-services/10-runit-control.sh
+++ core-services/10-runit-control.sh
@@ -4,3 +4,7 @@
 mkdir -p /run/runit
 install -m000 /dev/null /run/runit/stopit
 install -m000 /dev/null /run/runit/reboot
+
+# ensure runit keeps link count >0 if the binary is updated,
+# so the rootfs can be remounted read-only
+ln -f /bin/runit /bin/.runit.booted
